00e3c873407f280a4d61e8a5e0f6001d0ea1fc67
HuyAio <99306951+HuyAio@users.noreply.github.com>
2022-02-09 12:17:25 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "-Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý Ạ. <3",
   buttonNo: "Không! KHÔNG ĐƯỢC ĐÂU <3.",
-  titleModar: "Cảm ơn cậu vì đồng ý",
+  titleModar: "Cảm ơn cậu vì đồng ý tớ rất vui ",
   descriptionModar:
     'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Tớ chờ đợi lâu lắm rồi </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé đáng yêu </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi " </br> Cre: HuyAio',
 };
